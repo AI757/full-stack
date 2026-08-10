1ed28db9bcf011ae4712bee5a6e546159f93a4f8
Sfever <yanyoutong@outlook.com>
2026-08-10 14:30:52 -0700

    Add login and registration screens

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import AuthPage from './auth/AuthPage.jsx'
+import AuthStatus from './auth/AuthStatus.jsx'
 import Chatbot from './chatbot'
 import './App.css'
 
@@ -30,6 +32,7 @@ function Home() {
         >
           Count is {count}
         </button>
+        <AuthStatus />
       </section>
 
       <div className="ticks"></div>
@@ -126,6 +129,11 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/chat" element={<Chatbot />} />
+      <Route path="/login" element={<AuthPage key="login" mode="login" />} />
+      <Route
+        path="/register"
+        element={<AuthPage key="register" mode="register" />}
+      />
     </Routes>
   )
 }
